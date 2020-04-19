cc27d638c123d780cd7e96bf20d9280f93c46d30
Catalitium <catalitium@gmail.com>
2020-04-19 19:10:52 +0200

    Updates

---
+++ precache-manifest.5720dfac4f1eecc013a314d79546d9d6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9de977be476d869524ed3b81163a77",
+    "url": "/Catalitiumes/index.html"
+  },
+  {
+    "revision": "6c185de56bcbcad8c357",
+    "url": "/Catalitiumes/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "da90598c06106a4d33c6",
+    "url": "/Catalitiumes/static/css/main.9426a4e9.chunk.css"
+  },
+  {
+    "revision": "6c185de56bcbcad8c357",
+    "url": "/Catalitiumes/static/js/2.9020b6bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Catalitiumes/static/js/2.9020b6bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da90598c06106a4d33c6",
+    "url": "/Catalitiumes/static/js/main.58176558.chunk.js"
+  },
+  {
+    "revision": "f7fa5ef2901ceca6d80c",
+    "url": "/Catalitiumes/static/js/runtime-main.d9518037.js"
+  },
+  {
+    "revision": "d5b97135b12132610ca02ff0b190df7c",
+    "url": "/Catalitiumes/static/media/AALogo.d5b97135.png"
+  },
+  {
+    "revision": "f6c033360089fcdf362aefb603729bf1",
+    "url": "/Catalitiumes/static/media/AIcircle-cropped.f6c03336.png"
+  },
+  {
+    "revision": "b4597ef3041410d11360b8d7e66616b8",
+    "url": "/Catalitiumes/static/media/AWSLogo.b4597ef3.png"
+  },
+  {
+    "revision": "c7ab66687882302d1ef1c2ddc162f8d1",
+    "url": "/Catalitiumes/static/media/BotLogo.c7ab6668.png"
+  },
+  {
+    "revision": "8fab72c4f632b90b98b03d170d20bf46",
+    "url": "/Catalitiumes/static/media/Botcircle-cropped.8fab72c4.png"
+  },
+  {
+    "revision": "1e89f06eb755d9614ec233abc6d61ce8",
+    "url": "/Catalitiumes/static/media/CapLogo.1e89f06e.png"
+  },
+  {
+    "revision": "a1ef1c296b825df1cd0f3a36e4856563",
+    "url": "/Catalitiumes/static/media/Codecircle-cropped.a1ef1c29.png"
+  },
+  {
+    "revision": "59d81a18175fb47d28c4f2d9e94e1dee",
+    "url": "/Catalitiumes/static/media/InstagramLogo.59d81a18.png"
+  },
+  {
+    "revision": "239c240e6e23ecaa561943c7b905ef9d",
+    "url": "/Catalitiumes/static/media/MERNLogo.239c240e.png"
+  },
+  {
+    "revision": "75c36ef3660f11b4f12cfd4115f50bae",
+    "url": "/Catalitiumes/static/media/TwitterLogo.75c36ef3.png"
+  },
+  {
+    "revision": "de44c6ed5f61b39578816a149440e3c9",
+    "url": "/Catalitiumes/static/media/WebAppLogo.de44c6ed.png"
+  },
+  {
+    "revision": "707de248168a7ab269ddd240b2e49059",
+    "url": "/Catalitiumes/static/media/WebLogo.707de248.png"
+  },
+  {
+    "revision": "295a70b12b112c701f54b95face09219",
+    "url": "/Catalitiumes/static/media/logo.295a70b1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Catalitium-Es/precache-manifest.3e0b26ebaf5e08bf4255270639bbce04.js"
+  "/Catalitiumes/precache-manifest.5720dfac4f1eecc013a314d79546d9d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Catalitium-Es/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Catalitiumes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
